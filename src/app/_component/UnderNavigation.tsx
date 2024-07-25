72bec6b960a3027a1b70152453bd33c49eebaa4e
Baek, Sehee <tpgmlqor@gmail.com>
2024-07-25 18:10:20 +0900

    add: underNavigation 컴포넌트 생성

---
+++ src/app/_component/UnderNavigation.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import styles from "./underNavigation.module.css";
+import Link from "next/link";
+
+export default function UnderNavigation() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.navBar}>
+        {/* <Link href="/"> */}
+        <div className={styles.navItem}>글</div>
+        {/* </Link> */}
+        <Link href="/">
+          <div className={styles.navItem}>P</div>
+        </Link>
+        <Link href="/my">
+          <div className={styles.navItem}>마</div>
+        </Link>
+      </div>
+    </div>
+  );
+}
