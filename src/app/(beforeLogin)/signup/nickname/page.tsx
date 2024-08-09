116006523e7e8a65489cfac67b94104f8f1f9be2
Baek, Sehee <tpgmlqor@gmail.com>
2024-08-09 16:45:43 +0900

    refactor: 최초 로그인 시 설정 단계를 생략하여 로그인 플로우를 간소화

--- src/app/(beforeLogin)/signup/nickname/page.tsx
+++
@@ -1,9 +0,0 @@
-import NickNameMain from "@/app/_components/nickname/page";
-
-export default function NickName() {
-  return (
-    <>
-      <NickNameMain nextLink="/home" btn="등록하기" />
-    </>
-  );
-}
