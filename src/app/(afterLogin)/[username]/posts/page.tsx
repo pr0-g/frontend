c096432caa17f781d1b1af3eb3fe66b926352c69
Baek, Sehee <tpgmlqor@gmail.com>
2024-08-13 14:26:28 +0900

    add: 프로필 페이지

---
+++ src/app/(afterLogin)/[username]/posts/page.tsx
@@ -0,0 +1,10 @@
+import UnderNavigation from "../../_components/UnderNavigation";
+
+export default function Profile() {
+  return (
+    <div>
+      <UnderNavigation />
+      <div>프로필</div>
+    </div>
+  );
+}
