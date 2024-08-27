74b7e2c866cfa800aa4ad1680adfbe8634891f97
Baek, Sehee <tpgmlqor@gmail.com>
2024-08-27 19:11:00 +0900

    feat:  react-slick 설정 및 마이페이지에 적용

--- src/app/(afterLogin)/mypage/_components/SubscriptionWriter.tsx
+++ src/app/(afterLogin)/mypage/_components/SubscriptionWriter.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Image from "next/image";
-import styles from "./subscriptionWriter.module.css";
 import Link from "next/link";
+import Slider from "react-slick";
+import styles from "./subscriptionWriter.module.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 interface WriterItem {
   profileImage: string;
@@ -14,13 +17,53 @@ interface Props {
   viewAllLink: string;
 }
 
+function PrevArrow({ onClick }: { onClick?: () => void }) {
+  return (
+    <div className={`${styles.arrow} ${styles.prevArrow}`} onClick={onClick}>
+      {"<"}
+    </div>
+  );
+}
+
+function NextArrow({ onClick }: { onClick?: () => void }) {
+  return (
+    <div className={`${styles.arrow} ${styles.nextArrow}`} onClick={onClick}>
+      {">"}
+    </div>
+  );
+}
+
 export default function SubscriptionWriter({
   subscriptionItems,
   title,
   viewAllLink,
 }: Props) {
+  const settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    prevArrow: <PrevArrow />,
+    nextArrow: <NextArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+    ],
+  };
+
   return (
-    <div className={styles.subScriptionContainer}>
+    <div className={styles.subScriptionsContainer}>
       <div className={styles.header}>
         <p className={styles.title}>{title}</p>
         <p className={styles.viewAll}>
@@ -28,7 +71,7 @@ export default function SubscriptionWriter({
         </p>
       </div>
       <div className={styles.itemsWrap}>
-        <div className={styles.items}>
+        <Slider {...settings}>
           {subscriptionItems.map((item, index) => (
             <div key={index} className={styles.item}>
               <div className={styles.profileImageWrapper}>
@@ -43,7 +86,7 @@ export default function SubscriptionWriter({
               <p className={styles.writerName}>{item.name}</p>
             </div>
           ))}
-        </div>
+        </Slider>
       </div>
     </div>
   );
