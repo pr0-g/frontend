0355f3fbc51b07aeb2d85dfecc0c00c943edd114
Baek, Sehee <tpgmlqor@gmail.com>
2024-10-25 14:06:41 +0900

    feat: 로고 클릭시 탭 초기화 기능 추가

--- src/app/(afterLogin)/_components/Header.tsx
+++ src/app/(afterLogin)/_components/Header.tsx
@@ -1,11 +1,20 @@
 import Link from "next/link";
 import styles from "./header.module.css";
+import { useTabStore } from "@/store/tab";
 
 export default function Header() {
+  const setActiveTab = useTabStore((state) => state.setActiveTab);
+
+  const handleLogoClick = () => {
+    setActiveTab(0);
+  };
+
   return (
     <header className={styles.header}>
       <h1>
-        <Link href="/posts">PROG</Link>
+        <Link href="/posts" onClick={handleLogoClick}>
+          PROG
+        </Link>
       </h1>
     </header>
   );
